aa1e6070ae5f9bb52de6338ee860679510bef224
studiokrause <studiokrause@users.noreply.github.com>
2026-09-03 00:08:18 +0200

    NDI-discovery: update AppVersion to 1.0.1-NDI-discovery @ NDI-discovery

--- app/src/main/java/com/lekozaur/ndiviewer/AppVersion.kt
+++ app/src/main/java/com/lekozaur/ndiviewer/AppVersion.kt
@@ -2,6 +2,6 @@ package com.lekozaur.ndiviewer
 
 object AppVersion {
     const val VERSION = "1.0.1"
-    const val RELEASE = "v1.0.1"
-    const val BRANCH = "main"
+    const val RELEASE = "v1.0.1-NDI-discovery"
+    const val BRANCH = "NDI-discovery"
 }
